eadb7387e5aa3ebfbf626671bccfc9c106740b1e
mohanedbenmansour <mohaned.benmansour@esprit.tn>
2021-06-17 09:54:29 +0100

    fixing the server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,29 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-preview-app';
-color:string="http://localhost:8100/tabs/tab1?color=primary";
+color:string="https://ionic-nest-crud.herokuapp.com/tabs/tab1?color=primary";
 
 primary(){
-  this.color="http://localhost:8100/tabs/tab1?color=primary"
+  this.color="https://ionic-nest-crud.herokuapp.com/tabs/tab1?color=primary"
   
 }
 secondary(){
-  this.color="http://localhost:8100/tabs/tab1?color=secondary"
+  this.color="https://ionic-nest-crud.herokuapp.com/tabs/tab1?color=secondary"
 }
 success(){
-  this.color="http://localhost:8100/tabs/tab1?color=success"
+  this.color="https://ionic-nest-crud.herokuapp.com/tabs/tab1?color=success"
 }
 danger(){
-  this.color="http://localhost:8100/tabs/tab1?color=danger"
+  this.color="https://ionic-nest-crud.herokuapp.com/tabs/tab1?color=danger"
 }
 warning(){
-  this.color="http://localhost:8100/tabs/tab1?color=warning"
+  this.color="https://ionic-nest-crud.herokuapp.com/tabs/tab1?color=warning"
 }
 info(){
-  this.color="http://localhost:8100/tabs/tab1?color=info"
+  this.color="https://ionic-nest-crud.herokuapp.com/tabs/tab1?color=info"
 }
 random(){
-  this.color="http://localhost:8100/tabs/tab1?color=random"
+  this.color="https://ionic-nest-crud.herokuapp.com/tabs/tab1?color=random"
 }
 
 }
