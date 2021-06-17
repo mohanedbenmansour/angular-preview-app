b9529ea6106199f6094691787cebf5fade7cc18b
mohanedbenmansour <mohaned.benmansour@esprit.tn>
2021-06-17 07:50:59 +0100

    showing the preview in dev mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-preview-app';
+color:string="http://localhost:8100/tabs/tab1?color=primary";
+primary(){
+  this.color="http://localhost:8100/tabs/tab1?color=primary"
+}
+secondary(){
+  this.color="http://localhost:8100/tabs/tab1?color=secondary"
+}
+success(){
+  this.color="http://localhost:8100/tabs/tab1?color=success"
+}
+danger(){
+  this.color="http://localhost:8100/tabs/tab1?color=danger"
+}
+warning(){
+  this.color="http://localhost:8100/tabs/tab1?color=warning"
+}
+info(){
+  this.color="http://localhost:8100/tabs/tab1?color=info"
+}
 }
