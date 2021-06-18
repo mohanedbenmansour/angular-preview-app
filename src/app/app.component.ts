db9faa89a162c64cec1839e2dd9034ef448f1c88
mohanedbenmansour <mohaned.benmansour@esprit.tn>
2021-06-18 09:51:36 +0100

    finishing the first prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-preview-app';
-color:string="http://localhost:8100/tabs/tab1";
+color:string="https://ionic-nest-crud.herokuapp.com/tabs/tab1";
+viewClass:string="phone view_3";
 
 
 changeTheme(theme){
-this.color="http://localhost:8100/tabs/tab1?theme="+theme;
+this.color="https://ionic-nest-crud.herokuapp.com/tabs/tab1?theme="+theme;
+}
+
+view(name){
+  console.log(name)
+if(name=="laying"){
+  this.viewClass="phone view_1";
+
+}
+else if (name == "side"){
+  this.viewClass="phone view_2";
+
+}
+else if (name=='front'){
+  this.viewClass="phone view_3";
+
+}
 }
 
 }
